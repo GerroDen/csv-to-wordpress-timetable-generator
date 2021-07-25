e8e1380ec81b6313398b9f73c992a747d90a6146
Lars-Erik Kimmel <GerroDen@users.noreply.github.com>
2021-07-25 19:37:31 +0200

    Rewrite xml generator by using object format only

--- csv-to-wordpress-timetable.js
+++ csv-to-wordpress-timetable.js
@@ -26,56 +26,61 @@ async function csvToWordPressTimetable(csvFile) {
     const csv = await parseCsv(csvFile)
     console.info(`finished loading CSV with ${csv.length} lines`)
     const angebote = Object.keys(groupBy(csv, entry => entry.Angebot))
-    const doc = create({ version: "1.0", encoding: "UTF-8" })
-    const channel = doc.ele("rss")
-        .att("version", "2.0")
-        .att("xmlns:excerpt", "http://wordpress.org/export/1.2/excerpt/")
-        .att("xmlns:content", "http://purl.org/rss/1.0/modules/content/")
-        .att("xmlns:wfw", "http://wellformedweb.org/CommentAPI/")
-        .att("xmlns:dc", "http://purl.org/dc/elements/1.1/")
-        .att("xmlns:wp", "http://wordpress.org/export/1.2/")
-        .ele("channel")
-    weekdaysCsv.forEach((weekday, index) => {
-        channel.ele("item")
-        .ele("title").txt(weekday).up()
-        .ele("wp:post_id").txt(100 + index).up()
-        .ele("wp:post_name").dat(weekday.toLowerCase()).up()
-        .ele("wp:status").dat("publish").up()
-        .ele("wp:post_type").dat("mp-column").up()
-        .ele("wp:postmeta")
-        .ele("wp:meta_key").dat("_edit_last").up()
-        .ele("wp:meta_value").dat(1).up()
-        .up() // wp:postmeta
-        .ele("wp:postmeta")
-        .ele("wp:meta_key").dat("column_option").up()
-        .ele("wp:meta_value").dat("weekday").up()
-        .up() // wp:postmeta
-        .ele("wp:postmeta")
-        .ele("wp:meta_key").dat("weekday").up()
-        .ele("wp:meta_value").dat(weekdaysWordPress[index]).up()
-        .up() // wp:postmeta
-    })
-    angebote.forEach((angebot, index) => {
-        channel.ele("item")
-            .ele("title").txt(angebot).up()
-            .ele("wp:post_id").txt(200 + index).up()
-            .ele("wp:post_name").dat(angebot.toLowerCase().replace(/\s+/g, "-")).up()
-            .ele("wp:post_type").dat("mp-event").up()
-            .ele("wp:status").dat("publish").up()
-            .ele("wp:postmeta")
-            .ele("wp:meta_key").dat("timetable_disable_url").up()
-            .ele("wp:meta_value").dat(1).up()
-    })
-    csv.forEach((event) => {
-        channel.ele("timeslot")
-            .ele("column").dat(100 + weekdaysCsv.findIndex(weekday => weekday.startsWith(event.Wochentag))).up()
-            .ele("event").dat(200 + angebote.indexOf(event.Angebot)).up()
-            .ele("event_start").dat(event.Uhrzeit.split("-")[0]?.trim()).up()
-            .ele("event_end").dat(event.Uhrzeit.split("-")[1]?.trim()).up()
-            .ele("user_id").dat(-1).up()
-            .ele("description").dat(event.Alter).up()
-    })
-    return doc.end({ prettyPrint: true })
+    const xml = {
+        "rss": {
+            "@": {
+                "version": "2.0",
+                "xmlns:excerpt": "http://wordpress.org/export/1.2/excerpt/",
+                "xmlns:content": "http://purl.org/rss/1.0/modules/content/",
+                "xmlns:wfw": "http://wellformedweb.org/CommentAPI/",
+                "xmlns:dc": "http://purl.org/dc/elements/1.1/",
+                "xmlns:wp": "http://wordpress.org/export/1.2/",
+            },
+            "channel": {
+                "item": [
+                    ...weekdaysCsv.map((weekday, index) => ({
+                        "title": weekday,
+                        "wp:post_id": 100 + index,
+                        "wp:post_name": { "$": weekday.toLowerCase() },
+                        "wp:status": { "$": "publish" },
+                        "wp:post_type": { "$": "mp-column" },
+                        "wp:postmeta": [{
+                            "wp:meta_key": { "$": "_edit_last" },
+                            "wp:meta_value": { "$": 1 },
+                        }, {
+                            "wp:meta_key": { "$": "column_option" },
+                            "wp:meta_value": { "$": "weekday" },
+                        }, {
+                            "wp:meta_key": { "$": "weekday" },
+                            "wp:meta_value": { "$": weekdaysWordPress[index] },
+                        }],
+                    })),
+                    ...angebote.map((angebot, index) => ({
+                        "title": angebot,
+                        "wp:post_id": 200 + index,
+                        "wp:post_name": { "$": angebot.toLowerCase().replace(/\s+/g, "-") },
+                        "wp:post_type": { "$": "mp-event" },
+                        "wp:status": { "$": "publish" },
+                        "wp:postmeta": [{
+                            "wp:meta_key": { "$": "timetable_disable_url" },
+                            "wp:meta_value": { "$": 1 },
+                        }],
+                    })),
+                ],
+                "timeslot": csv.map(event => ({
+                    "column": { "$": 100 + weekdaysCsv.findIndex(weekday => weekday.startsWith(event.Wochentag)) },
+                    "event": { "$": 200 + angebote.indexOf(event.Angebot) },
+                    "event_start": { "$": event.Uhrzeit.split("-")[0]?.trim() + ":00" },
+                    "event_end": { "$": event.Uhrzeit.split("-")[1]?.trim() + ":00" },
+                    "user_id": { "$": -1 },
+                    "description": { "$": event.Alter },
+                })),
+            },
+        },
+    };
+    return create({ version: "1.0", encoding: "UTF-8" })
+        .ele(xml)
+        .end({ prettyPrint: true })
 }
 
 function parseCsv(csvFile) {
