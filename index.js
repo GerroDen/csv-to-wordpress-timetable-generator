1dc2d7f6a75624ea6ff7506bf6a677a3a394671a
Lars-Erik Kimmel <GerroDen@users.noreply.github.com>
2021-07-31 21:41:24 +0200

    Write CLI commands with yargs

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 #!/usr/bin/env node
-const { csvToWordPressTimetable } = require("./csv-to-wordpress-timetable");
 const fs = require("fs").promises;
+const yargs = require("yargs/yargs");
+const { csvToWordPressTimetable } = require("./csv-to-wordpress-timetable");
 
 (async function () {
-    const csvFile = process.argv[2];
-    const xmlFile = process.argv[3];
-    const xml = await csvToWordPressTimetable({ csvFile, xmlFile });
-    console.log(xml);
+    const argv = yargs(process.argv.slice(2))
+        .usage("Usage: $0 --csv [file] --xml [file] --out [file]")
+        .options({
+            csv: {
+                string: true,
+                demandOption: true,
+                desc: "CSV export from Google Sheet",
+            },
+            xml: {
+                string: true,
+                demandOption: true,
+                desc: "XML export from WordPress timetable plugin",
+            },
+            out: {
+                string: true,
+                demandOption: true,
+                desc: "output file path",
+            },
+        })
+        .argv;
+    const xml = await csvToWordPressTimetable({
+        csvFile: argv.csv,
+        xmlFile: argv.xml,
+    });
+    await fs.writeFile(argv.out, xml, { encoding: "utf8" });
 })();
