3fd01fc01df0f93aa15398847f1a4a3f40278572
Lars-Erik Kimmel <GerroDen@users.noreply.github.com>
2021-07-31 21:01:17 +0200

    Generate import xml from CSV

--- csv-to-wordpress-timetable.js
+++ csv-to-wordpress-timetable.js
@@ -1,12 +1,12 @@
 const fs = require("fs")
 const csvParser = require("csv-parser")
 const { create } = require("xmlbuilder2")
-const { groupBy } = require("lodash")
+const { groupBy, cloneDeep, zipObject, chain } = require("lodash")
 const { convert } = require("xmlbuilder2")
 
 class CsvAccessor {
     constructor(csv) {
-        this.csv = csv
+        this.data = csv.map(line => new CsvLineAccessor(line))
     }
 
     static async parse(csvFile) {
@@ -15,7 +15,7 @@ class CsvAccessor {
     }
 
     get angebotMap() {
-        return groupBy(this.csv, entry => entry.Angebot)
+        return groupBy(this.data, entry => entry.Angebot)
     }
 
     get angebote() {
@@ -23,27 +23,47 @@ class CsvAccessor {
     }
 
     get length() {
-        return this.csv.length
+        return this.data.length
     }
 
-    [Symbol.iterator]() { 
-        return this.csv.values() 
+    [Symbol.iterator]() {
+        return this.data.values()
     }
 
     static weekdays = [
-        "Montags",
-        "Dienstags",
-        "Mittwochs",
-        "Donnerstags",
-        "Freitags",
-        "Samstags",
-        "Sonntags",
+        "Montag",
+        "Dienstag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Samstag",
+        "Sonntag",
     ]
+}
+
+class CsvLineAccessor {
+    constructor(line) {
+        for (const key in line) {
+            this[key] = line[key]
+        }
+    }
+
+    get timeslot() {
+        const parts = this.Uhrzeit.split("-")
+        return {
+            start: parts[0]?.trim(),
+            end: parts[1]?.trim(),
+        }
+    }
+
+    get timeslotComplete() {
+        return this.timeslot.start !== undefined && this.timeslot.end !== undefined
+    }
 }
 
 class XmlAccessor {
     constructor(xml) {
-        this.xml = xml
+        this.data = xml
     }
 
     static async parse(xmlFile) {
@@ -53,11 +73,11 @@ class XmlAccessor {
     }
 
     get items() {
-        return this.xml.rss.channel.item
+        return this.data.rss.channel.item
     }
 
     get timeslots() {
-        return this.xml.rss.channel.timeslot
+        return this.data.rss.channel.timeslot
     }
 
     get weekdayItems() {
@@ -65,10 +85,10 @@ class XmlAccessor {
     }
 
     get weekdayItemMap() {
-        return groupBy(
-            this.weekdayItems,
-            item => item["wp:postmeta"].find(meta => meta["wp:meta_key"]["$"] === "weekday")["wp:meta_value"]["$"]
-        )
+        return chain(this.weekdayItems)
+            .groupBy(item => item["wp:postmeta"].find(meta => meta["wp:meta_key"]["$"] === "weekday")["wp:meta_value"]["$"])
+            .mapValues(values => values[0])
+            .value()
     }
 
     get weekdays() {
@@ -80,7 +100,10 @@ class XmlAccessor {
     }
 
     get angebotItemMap() {
-        return groupBy(this.angebotItems, item => item.title)
+        return chain(this.angebotItems)
+            .groupBy(item => item.title)
+            .mapValues(values => values[0])
+            .value()
     }
 
     get angebote() {
@@ -98,65 +121,77 @@ class XmlAccessor {
     ]
 }
 
+class XmlToCsv {
+    /**
+     * @param {object} params
+     * @param {XmlAccessor} params.xml
+     * @param {CsvAccessor} params.csv
+     */
+    constructor({ xml, csv }) {
+        this.xml = xml
+        this.csv = csv
+        this.weekdaysCsvToXml = zipObject(CsvAccessor.weekdays, XmlAccessor.weekdays)
+    }
+
+    generate() {
+        const xml = this.generateBaseXml()
+        xml.rss.channel.item.push(...cloneDeep(this.xml.weekdayItems))
+        xml.rss.channel.item.push(...this.csv.data.map(this.generateAngebot.bind(this)))
+        xml.rss.channel.timeslot.push(...this.csv.data.filter(line => line.timeslotComplete).map(this.generateTimeslot.bind(this)))
+        const removedAngebote = this.xml.angebote.filter(xmlAngebot => !this.csv.angebote.includes(xmlAngebot))
+        if (removedAngebote.length > 0) console.info(`removed angebote ${removedAngebote}`)
+        const addedAngebote = this.csv.angebote.filter(csvAngebot => !this.xml.angebote.includes(csvAngebot))
+        if (addedAngebote.length > 0) console.info(`added angebote ${addedAngebote}`)
+        console.info(`number of timeslots changed from ${this.xml.timeslots.length} to ${new XmlAccessor(xml).timeslots.length}`)
+        return xml
+    }
+
+    generateBaseXml() {
+        const baseXml = cloneDeep(this.xml.data);
+        baseXml.rss.channel.item = []
+        baseXml.rss.channel.timeslot = []
+        return baseXml
+    }
+
+    /**
+     * @param {CsvLineAccessor} csvLine
+     * @param {number} index
+     */
+    generateAngebot(csvLine, index) {
+        return {
+            ...cloneDeep(this.xml.angebotItems[0]),
+            "title": csvLine.Angebot,
+            "wp:post_id": this.xml.angebotItemMap[csvLine.Angebot]["wp:post_id"] ?? 100 + index,
+            "wp:post_name": { "$": csvLine.Angebot.toLowerCase().replace(/\s+/g, "-") },
+        };
+    }
+
+    /**
+     * @param {CsvLineAccessor} csvLine
+     * @param {number} index
+     */
+    generateTimeslot(csvLine, index) {
+        const xmlWeekday = this.weekdaysCsvToXml[csvLine.Wochentag];
+        return {
+            ...cloneDeep(this.xml.timeslots[0]),
+            "column": { "$": this.xml.weekdayItemMap[xmlWeekday]["wp:post_id"] },
+            "event": { "$": this.xml.angebotItemMap[csvLine.Angebot]["wp:post_id"] ?? 100 + index },
+            "event_start": { "$": csvLine.timeslot.start + ":00" },
+            "event_end": { "$": csvLine.timeslot.end + ":00" },
+            "description": { "$": csvLine.Alter },
+        }
+    }
+}
+
 async function csvToWordPressTimetable({ csvFile, xmlFile }) {
     const csv = await CsvAccessor.parse(csvFile)
     console.info(`finished loading CSV with ${csv.length} lines`)
     const xml = await XmlAccessor.parse(xmlFile)
     console.info(`loaded base timetable from XML with ${xml.timeslots.length} timeslots`)
-    const removedAngebote = xml.angebote.filter(xmlAngebot => !csv.angebote.includes(xmlAngebot))
-    const addedAngebote = csv.angebote.filter(csvAngebot => !xml.angebote.includes(csvAngebot))
-    const removedTimeslots = ""
-    const addedTimeslots = ""
-    // console.log(Object.keys(weekdayItemIndex))
-    // console.log(Object.keys(angebotItemIndex))
-    /*
-    const xml = {
-        "rss": {
-            "channel": {
-                "item": [
-                    ...weekdaysCsv.map((weekday, index) => ({
-                        "title": weekday,
-                        "wp:post_id": 100 + index,
-                        "wp:post_name": { "$": weekday.toLowerCase() },
-                        "wp:status": { "$": "publish" },
-                        "wp:post_type": { "$": "mp-column" },
-                        "wp:postmeta": [{
-                            "wp:meta_key": { "$": "_edit_last" },
-                            "wp:meta_value": { "$": 1 },
-                        }, {
-                            "wp:meta_key": { "$": "column_option" },
-                            "wp:meta_value": { "$": "weekday" },
-                        }, {
-                            "wp:meta_key": { "$": "weekday" },
-                            "wp:meta_value": { "$": weekdaysWordPress[index] },
-                        }],
-                    })),
-                    ...angebote.map((angebot, index) => ({
-                        "title": angebot,
-                        "wp:post_id": 200 + index,
-                        "wp:post_name": { "$": angebot.toLowerCase().replace(/\s+/g, "-") },
-                        "wp:post_type": { "$": "mp-event" },
-                        "wp:status": { "$": "publish" },
-                        "wp:postmeta": [{
-                            "wp:meta_key": { "$": "timetable_disable_url" },
-                            "wp:meta_value": { "$": 1 },
-                        }],
-                    })),
-                ],
-                "timeslot": csv.map(event => ({
-                    "column": { "$": 100 + weekdaysCsv.findIndex(weekday => weekday.startsWith(event.Wochentag)) },
-                    "event": { "$": 200 + angebote.indexOf(event.Angebot) },
-                    "event_start": { "$": event.Uhrzeit.split("-")[0]?.trim() + ":00" },
-                    "event_end": { "$": event.Uhrzeit.split("-")[1]?.trim() + ":00" },
-                    "user_id": { "$": -1 },
-                    "description": { "$": event.Alter },
-                })),
-            },
-        },
-    }
-    */
+    const xmlToCsv = new XmlToCsv({ xml, csv })
+    const newXml = xmlToCsv.generate()
     return create({ version: "1.0", encoding: "UTF-8" })
-        .ele(xml)
+        .ele(newXml)
         .end({ prettyPrint: true })
 }
 
--- csv-to-wordpress-timetable.test.js
+++ csv-to-wordpress-timetable.test.js
@@ -1,18 +1,15 @@
-const fs = require("fs").promises;
-const path = require("path");
-const { describe, expect, it } = require("@jest/globals");
-const { convert } = require("xmlbuilder2");
-const { csvToWordPressTimetable } = require("./csv-to-wordpress-timetable");
+const fs = require("fs").promises
+const path = require("path")
+const { describe, expect, it } = require("@jest/globals")
+const { csvToWordPressTimetable } = require("./csv-to-wordpress-timetable")
 
-const csvFile = path.resolve(__dirname, "__fixtures__/timetable.csv");
-const xmlFile = path.resolve(__dirname, "__fixtures__/timetable_export.xml");
+const csvFile = path.resolve(__dirname, "__fixtures__/timetable.csv")
+const xmlFile = path.resolve(__dirname, "__fixtures__/timetable_export.xml")
 
 describe("csvToWordPressTimetable", () => {
     it("converts csv to wordpress timetable", async () => {
-        const xml = await fs.readFile(xmlFile, { encoding: "utf8" });
+        const result = await csvToWordPressTimetable({ csvFile, xmlFile })
 
-        const result = await csvToWordPressTimetable({ csvFile, xmlFile });
-
-        expect(result).toMatchSnapshot();
+        expect(result).toMatchSnapshot()
     });
 });
